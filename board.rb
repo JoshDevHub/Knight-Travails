bca005a8fbca5ecae34b58ac1344c2ddc53d18de
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-11 11:26:36 -0700

    Add class boilerplate

---
+++ board.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# class that functions as a graph structure
+class Board
+  # placeholder
+end
---
+++ knight.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# it moves like a knight and ... quacks like a knight?
+class Knight
+  # placeholder
+end
