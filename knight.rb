c300f5d2e243cf455dff5ea739ebda635e3d9447
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-11 14:28:44 -0700

    Add #find_moves, which tells the 8 coordinates a knight can travel to

--- knight.rb
+++ knight.rb
@@ -2,5 +2,16 @@
 
 # it moves like a knight and ... quacks like a knight?
 class Knight
-  # placeholder
+  def find_moves(coordinate_position)
+    [
+      coordinate_position + (1 + 2i),
+      coordinate_position + (1 - 2i),
+      coordinate_position + (2 + 1i),
+      coordinate_position + (2 - 1i),
+      coordinate_position + (-1 + 2i),
+      coordinate_position + (-1 - 2i),
+      coordinate_position + (-2 + 1i),
+      coordinate_position + (-2 - 1i)
+    ]
+  end
 end
