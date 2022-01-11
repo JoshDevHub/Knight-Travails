7ab6c8310e8be2cd42b37884bc048c62457fbe29
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-11 15:25:30 -0700

    Add @distance and @pred to Square to prepare for implementing search algorithm

--- square.rb
+++ square.rb
@@ -2,9 +2,15 @@
 
 # A single square position on a chess board
 class Square
-  attr_reader :name
+  attr_accessor :name, :distance, :pred
 
-  def initialize(name)
+  def initialize(name, distance = nil, pred = nil)
     @name = name
+    @distance = distance
+    @pred = pred
+  end
+
+  def visited?
+    !distance.nil?
   end
 end
