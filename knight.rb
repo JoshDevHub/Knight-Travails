d1b5f2944a6e898ce9d3ac206fc88c7e673d4789
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-12 09:19:42 -0700

    Clean up knight class

--- knight.rb
+++ knight.rb
@@ -2,16 +2,15 @@
 
 # it moves like a knight and ... quacks like a knight?
 class Knight
-  def generate_moves(coordinate_position)
-    [
-      coordinate_position + (1 + 2i),
-      coordinate_position + (1 - 2i),
-      coordinate_position + (2 + 1i),
-      coordinate_position + (2 - 1i),
-      coordinate_position + (-1 + 2i),
-      coordinate_position + (-1 - 2i),
-      coordinate_position + (-2 + 1i),
-      coordinate_position + (-2 - 1i)
-    ]
+  def generate_moves(coordinate)
+    move_coordinates.map do |move|
+      [coordinate, move].transpose.map(&:sum)
+    end
+  end
+
+  def move_coordinates
+    x = [2, 2, 1, 1, -1, -1, -2, -2]
+    y = [1, -1, 2, -2, 2, -2, 1, -1]
+    x.zip(y)
   end
 end
